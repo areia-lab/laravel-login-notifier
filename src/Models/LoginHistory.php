f737ac843fe02862e044b7f89663067364c974f2
engr-akramulhoque <akramulhoque.enigneer@gmail.com>
2025-09-13 19:47:54 +0600

    Modified Code and Docs added

--- src/Models/LoginHistory.php
+++ src/Models/LoginHistory.php
@@ -2,11 +2,23 @@
 
 namespace AreiaLab\LoginNotifier\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class LoginHistory extends Model
 {
     protected $fillable = [
-        'user_id', 'ip_address', 'device', 'platform', 'browser', 'geo_location'
+        'user_id',
+        'ip_address',
+        'device',
+        'platform',
+        'browser',
+        'geo_location'
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
